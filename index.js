002974132557fb96ccc327d8f2ca855d30d69030
ikki1234568 <153141077+ikki1234568@users.noreply.github.com>
2023-12-16 04:34:04 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -15,3 +15,46 @@ function updateServerSpecs(serverSpecsId) {
     document.getElementById("server2Specs").style.display = "none";
     document.getElementById(serverSpecsId).style.display = "block";
 }
+/*rustのサーバーに入ってる人のやつ*/
+async function popStatus(serverId, elementId) {
+    const url = `https://api.battlemetrics.com/servers/${serverId}`;
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            const status = data.data.attributes.status.toLowerCase();
+            const connectedPlayers = data.data.attributes.players;
+            const maxPlayers = data.data.attributes.maxPlayers;
+            const queuedPlayers = data.data.attributes.details.rust_queued_players || 0;
+
+            let statusText = "";
+            if (status === "online" || status === "playing") {
+                if (queuedPlayers > 0) {
+                    statusText = `${connectedPlayers}/${maxPlayers} (+${queuedPlayers})`;
+                } else {
+                    statusText = `${connectedPlayers}/${maxPlayers} `;
+                }
+            } else {
+                statusText = "[Offline]";
+            }
+
+            document.getElementById(elementId).innerText = ` ${statusText}`;
+        } else {
+            console.error(`Battlemetrics Error with status code: ${response.status}`);
+            document.getElementById(elementId).innerText = `Battlemetrics Error -> ${response.status}`;
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+// Call the function for each server when the page loads
+popStatus("24768964", "server-status");
+popStatus("24807998", "server-status2");
+
+// Optionally, you can set up a periodic refresh
+setInterval(() => {
+    popStatus("24768964", "server-status");
+    popStatus("24807998", "server-status2");
+}, 60000);
